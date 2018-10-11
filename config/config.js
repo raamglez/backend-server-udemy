757a49490f1e7013a14c4a1ead8745ee8f901cb4
Rafael Amaya González <rafa.amaya.gles@gmail.com>
2018-10-11 13:08:36 -0500

    Sección 12 terminada Back-End terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.CLIENT_ID = '36180254334-6mamo0b93878b1n8pu11l9lc3fov2dqf.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,56 @@
 let express  = require('express');
 const bcrypt = require('bcrypt');
 const jwt    = require('jsonwebtoken');
-
-const SEED = require('../config/config').SEED;
+const SEED   = require('../config/config').SEED;
 
 let Usuario = require('../models/usuario');
 let app     = express();
 
+//Google
+const CLIENT_ID        = require('../config/config').CLIENT_ID;
+const { OAuth2Client } = require('google-auth-library');
+const client           = new OAuth2Client(CLIENT_ID);
+
+// Autenticacion de Google
+async function verify(token) {
+    const ticket  = await client.verifyIdToken({
+        idToken : token,
+        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    // const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+
+    return {
+        nombre: payload.name,
+        email : payload.email,
+        img   : payload.picture,
+        google: payload.name,
+    }
+}
+
+app.post('/google', async (req, res) => {
+
+    let token = req.body.token;
+
+    let googleUser = await verify(token).catch(e => {
+        res.status(403).json({
+            ok     : false,
+            mensaje: "Token no válido"
+        })
+    });
+
+    res.status(200).json({
+        ok     : true,
+        mensaje: "OK",
+        googleUser: googleUser
+    })
+});
+
+// Autenticación normal
 app.post('/', (req, res) => {
 
     const body = req.body;
